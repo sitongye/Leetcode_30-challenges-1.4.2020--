1dc2201435a299f07dd6ddbc034f672bc99636da
sitongye <43437160+sitongye@users.noreply.github.com>
2020-04-08 20:40:28 +0200

    Create Day1_Single Number.py

---
+++ Day1_Single Number.py
@@ -0,0 +1,14 @@
+# Given a non-empty array of integers, every element appears twice except for one. Find that single one.
+# requirement: linear runtime complexity
+
+# example input:[2,2,1] output: 1
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        x = 0
+        for i in nums:
+            x = x^i
+        return x
+
+# This is a XOR solution, any number with an xor with itself results in 0, thus, number with two occurence will be zero after XOR,
+any number with zero with be itself after XOR, thus the return will be that number after operating XOR on the whole sequence 
